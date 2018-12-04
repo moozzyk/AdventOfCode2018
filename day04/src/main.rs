35ab0ee2a8fc0f23b97382e9080446382b932c33
moozzyk <moozzyk@gmail.com>
2018-12-03 23:57:18 -0800

    Day 4, Problem 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -78,6 +78,56 @@ fn problem1() {
     println!("{}", guard_id.parse::<i32>().unwrap() * max_freq_idx as i32);
 }
 
+// TODO: need to understand lifetimes to be able to extract these methods (i.e. returning HashMap)
+fn problem2() {
+    let mut lines = lines_from_file("input.txt");
+    lines.sort_unstable();
+
+    let mut sleep_times = HashMap::new();
+
+    let mut line_idx = 0;
+    while line_idx < lines.len() {
+        let guard_id = &lines[line_idx].split(' ').collect::<Vec<&str>>()[3][1..];
+        line_idx += 1;
+
+        if !sleep_times.contains_key(&guard_id) {
+            sleep_times.insert(guard_id, Vec::new());
+        }
+
+        while line_idx < lines.len() && !&lines[line_idx].contains("Guard") {
+            let interval = get_interval(&lines[line_idx], &lines[line_idx + 1]);
+            sleep_times.get_mut(&guard_id).unwrap().push(interval);
+            line_idx += 2;
+        }
+    }
+
+    let mut guard_id = "";
+    let mut max_minute_freq = 0;
+    let mut max_minute_freq_idx = 0;
+
+    for (g, intervals) in &sleep_times {
+
+        let mut minute_freq = [0; 60];
+        for interval in intervals {
+            let (from, to) = interval;
+            for minute in *from..*to {
+                minute_freq[minute as usize] += 1;
+            }
+        }
+
+        for idx in 0usize..60usize {
+            if minute_freq[idx] > max_minute_freq {
+                max_minute_freq = minute_freq[idx];
+                max_minute_freq_idx = idx;
+                guard_id = g;
+            }
+        }
+    }
+
+    println!("{}", guard_id.parse::<i32>().unwrap() * max_minute_freq_idx as i32);
+}
+
 fn main() {
     problem1();
+    problem2();
 }
