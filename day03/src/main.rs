48cd7ff426d525c6ad4bf4dcaaddeddaf40884f4
moozzyk <moozzyk@gmail.com>
2018-12-03 20:13:51 -0800

    Day 3, Problem 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -5,6 +5,7 @@ use std::io::BufReader;
 use std::fs::File;
 use std::path::Path;
 use regex::Regex;
+use std::collections::HashSet;
 
 fn lines_from_file<P>(filename: P) -> Vec<String>
 where
@@ -48,6 +49,37 @@ fn problem1() {
     println!("{}", count);
 }
 
+fn problem2() {
+    let lines = lines_from_file("input.txt");
+
+    let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    let mut fabric = [[0; 1000]; 1000];
+    let mut ids: HashSet<i32> = HashSet::new();
+
+    for line in lines.iter() {
+        let caps = re.captures(line).unwrap();
+        let id: i32 = caps.get(1).map(|c| c.as_str().parse().unwrap()).unwrap();
+        let start_row: usize = caps.get(2).map(|c| c.as_str().parse().unwrap()).unwrap();
+        let start_col: usize = caps.get(3).map(|c| c.as_str().parse().unwrap()).unwrap();
+        let width: usize = caps.get(4).map(|c| c.as_str().parse().unwrap()).unwrap();
+        let height: usize = caps.get(5).map(|c| c.as_str().parse().unwrap()).unwrap();
+
+        ids.insert(id);
+        for row in start_row..start_row + width {
+            for col in start_col..start_col + height {
+                if fabric[row][col] != 0 {
+                    ids.remove(&id);
+                    ids.remove(&fabric[row][col]);
+                }
+                fabric[row][col] = id;
+            }
+        }
+    }
+
+    println!("{:?}", ids);
+}
+
 fn main() {
-    problem1()
+    problem1();
+    problem2();
 }
