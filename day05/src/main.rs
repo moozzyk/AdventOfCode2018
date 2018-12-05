df49f6f96e9c7e6b4b7efc8d39ec188bfa904d6a
moozzyk <moozzyk@gmail.com>
2018-12-05 00:25:42 -0800

    Day 5, Problem 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -2,11 +2,10 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 use std::path::Path;
+use std::cmp;
 
-fn read_first_line<P>(filename: P) -> String
-where
-    P: AsRef<Path>,
-{
+fn read_first_line<P>(filename: P) -> String where
+    P: AsRef<Path> {
     let file = File::open(filename).expect("no such file");
     let mut buf = BufReader::new(file);
 
@@ -15,22 +14,39 @@ where
     return line;
 }
 
-fn problem1() {
-    let mut line = read_first_line("input.txt");
-    line.pop(); // remove trailing \n
-
+fn problem<F>(line: &String, skip: F) -> usize where
+    F: Fn(u8) -> bool {
     let chars: Vec<u8> = line.as_bytes().to_vec();
     let mut stack = Vec::new();
     for c in chars {
-        if stack.len() == 0 || (stack[stack.len() - 1] != c - 32 && stack[stack.len() - 1] != c + 32) {
-            stack.push(c);
-        } else {
-            stack.pop();
+        if !skip(c) {
+            if stack.len() == 0 || (stack[stack.len() - 1] != c - 32 && stack[stack.len() - 1] != c + 32) {
+                stack.push(c);
+            } else {
+                stack.pop();
+            }
         }
     }
-    println!("{}", stack.len());
+    return stack.len();
+}
+
+fn problem1(line: &String) {
+    println!("{}", problem(&line, |_c| false));
+}
+
+fn problem2(line: &String) {
+    let mut min_len = line.len() + 1;
+    for c in 97..123 /* 'a' to 'z' */ {
+        let len = problem(&line, |ch| c == ch | 0b00100000);
+        min_len = cmp::min(min_len, len);
+    }
+    println!("{}", min_len);
 }
 
 fn main() {
-    problem1();
+    let mut line = read_first_line("input.txt");
+    line.pop(); // remove trailing \n
+
+    problem1(&line);
+    problem2(&line);
 }
