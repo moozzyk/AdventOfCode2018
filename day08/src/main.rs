6319a6b6b9a7392da011861badecb18b84030a0c
moozzyk <moozzyk@gmail.com>
2018-12-08 08:49:11 -0800

    Day 8, Problem 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -31,18 +31,49 @@ fn metadata_sum(input: &Vec<u32>, mut index: usize) -> (u32, usize) {
     return (sum, index);
 }
 
-fn problem1(input: &str) {
-    let tree = input
+fn problem1(tree: &Vec<u32>) -> u32 {
+    let (metadata_sum, _) = metadata_sum(&tree, 0);
+    return metadata_sum;
+}
+
+fn metadata_sum_2(tree: &Vec<u32>, mut index: usize) -> (u32, usize) {
+    let num_nodes = tree[index];
+    let num_metadata = tree[index + 1] as usize;
+    index += 2;
+
+    if num_nodes == 0 {
+        let sum = tree[index..index + num_metadata].iter().sum();
+        index += num_metadata;
+        return (sum, index);
+    }
+
+    let mut node_sum = Vec::new();
+    for _i in 0..num_nodes {
+        let (sum, new_index) = metadata_sum_2(&tree, index);
+        node_sum.push(sum);
+        index = new_index;
+    }
+
+    let mut sum = 0;
+    for i in index..index + num_metadata {
+        sum += node_sum.get((tree[i] - 1) as usize).unwrap_or(&0);
+    }
+
+    index += num_metadata;
+    return (sum, index);
+}
+
+fn problem2(tree: &Vec<u32>) -> u32 {
+    let (result, _) = metadata_sum_2(tree, 0);
+    return result;
+}
+
+fn main() {
+    let input = read_first_line("input.txt")
         .split_whitespace()
         .map(|s| s.parse::<u32>().unwrap())
         .collect::<Vec<u32>>();
 
-    let (metadata_sum, _) = metadata_sum(&tree, 0);
-
-    println!("{}", metadata_sum);
-}
-
-fn main() {
-    let input = read_first_line("input.txt");
-    problem1(&input);
+    println!("{}", problem1(&input));
+    println!("{}", problem2(&input));
 }
